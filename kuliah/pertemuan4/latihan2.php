e84b16f826782ef9e130f42829827390685d34e6
FNKJ007 <m.haekalalghazy078@gmail.com>
2024-03-03 15:32:00 +0700

    MENYELESAIKAN TUGAS 3

---
+++ kuliah/pertemuan4/latihan2.php
@@ -0,0 +1,9 @@
+<?php
+// sisi kubus a = 9, sisi kubus b = 4//
+// hitung total volume kedua kubus//
+$sisi_a = 9;
+$sisi_b = 4;
+$volume_a = $sisi_a * $sisi_a * $sisi_a;
+$volume_b = $sisi_b * $sisi_b * $sisi_b;
+$total = $volume_a + $volume_b;
+echo "total volume kubus A & B adalah = $total";
